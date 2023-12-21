f070d86726fa096f8121c5dafce5e779bb44a5bd
Sadiqhasan <sadiqhasanrupani11@gmail.com>
2023-12-21 16:18:38 +0530

    created a basic bank console application

--- bank.go
+++ bank.go
@@ -7,44 +7,47 @@ import (
 func main() {
 	var accountBalance float64 = 6000
 
-	outputNextLine("\n" + `Welcome to "Go Bank"! 🏦`)
-	outputNextLine("\nWhat do you want to do?")
-	outputNextLine("\n1. Check Balance")
-	outputNextLine(`2. Deposit Money`)
-	outputNextLine(`3. Withdraw Money`)
-	outputNextLine(`4. Exit`)
+	for {
+		outputNextLine("\n" + `Welcome to "Go Bank"! 🏦`)
+		outputNextLine("\nWhat do you want to do?")
+		outputNextLine("\n1. Check Balance")
+		outputNextLine(`2. Deposit Money`)
+		outputNextLine(`3. Withdraw Money`)
+		outputNextLine(`4. Exit`)
 
-	choice := getIntUserInput("\nYour choice: ")
+		choice := getIntUserInput("\nYour choice: ")
 
-	if choice == 1 {
-		fmt.Printf("\nYour balance is: %v\n", accountBalance)
-	} else if choice == 2 {
-		deposit := getFloatUserInput("\nYour Deposit: ")
+		if choice == 1 {
+			fmt.Printf("\nYour balance is: %v\n", accountBalance)
+		} else if choice == 2 {
+			deposit := getFloatUserInput("\nYour Deposit: ")
 
-		if deposit <= 0 {
-			outputNextLine("\nInvalid amount. value must be greater than 0.")
-			return
+			if deposit <= 0 {
+				outputNextLine("\nInvalid amount. value must be greater than 0.")
+				return
+			}
+
+			accountBalance += deposit
+			fmt.Printf("\nBalance Updated! New amount is: %v\n", accountBalance)
+		} else if choice == 3 {
+			withdraw := getFloatUserInput("\nYour Withdraw Money: ")
+			accountBalance -= withdraw
+
+			if withdraw <= 0 {
+				outputNextLine("\nInvalid amount. value must be greater than 0.")
+				return
+			}
+
+			if withdraw > accountBalance {
+				outputNextLine("\nInvalid amount. You can't withdraw more than you have.")
+				return
+			}
+
+			fmt.Printf("\nBalance Updated! New amount is: %v\n", accountBalance)
+		} else if choice == 4 {
+			outputNextLine("\nAdieu! 🌟 Return for more adventures! 🚀")
+			break
 		}
-
-		accountBalance += deposit
-		fmt.Printf("\nBalance Updated! New amount is: %v\n", accountBalance)
-	} else if choice == 3 {
-		withdraw := getFloatUserInput("\nYour Withdraw Money: ")
-		accountBalance -= withdraw
-
-		if withdraw <= 0 {
-			outputNextLine("\nInvalid amount. value must be greater than 0.")
-			return
-		}
-
-		if withdraw > accountBalance {
-			outputNextLine("\nInvalid amount. You can't withdraw more than you have.")
-			return
-		}
-
-		fmt.Printf("\nBalance Updated! New amount is: %v\n", accountBalance)
-	} else {
-		outputNextLine("\nAdieu! 🌟 Return for more adventures! 🚀")
 	}
 }
 
